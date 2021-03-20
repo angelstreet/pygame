d6ac1f8e372751f1c5806d0ebfdf76db0490a44d
joachim <joachim_djibril@hotmail.com>
2021-03-20 12:59:24 +0100

    multi keyboard binding

--- my_games/tilemap/main.py
+++ my_games/tilemap/main.py
@@ -9,7 +9,14 @@ WHITE = (255, 255, 255)
 TITLE = "Isomap"
 MAP_X, MAP_Y = 250, 100
 PLAYER_X, PLAYER_Y = 530,140
+KEYBOARD = "AZERTY"
+
 # FUNCTIONS----------------------
+def get_keyboard_keys():
+    if KEYBOARD=="AZERTY" :
+        return pygame.K_q,pygame.K_d,pygame.K_z,pygame.K_s
+    return pygame.K_a,pygame.K_d,pygame.K_w,pygame.K_s
+
 def draw_game(screen,display,all_sprite,map):
     display.fill(WHITE)
     display.blit(map.image, (map.rect.x, map.rect.y))
@@ -34,30 +41,31 @@ def main():
     all_sprite.add(player)
     running = True
     clock = pygame.time.Clock()
+    K_LEFT, K_RIGHT,K_UP,K_DOWN = get_keyboard_keys()
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
                 return pygame.quit()
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
+                if event.key == pygame.K_LEFT or event.key == K_LEFT:
                      player.K_LEFT = True
-                elif event.key == pygame.K_RIGHT:
+                elif event.key == pygame.K_RIGHT or event.key == K_RIGHT:
                      player.K_RIGHT = True
-                if event.key == pygame.K_UP:
+                if event.key == pygame.K_UP or event.key == K_UP:
                     player.K_UP = True
-                elif event.key == pygame.K_DOWN:
+                elif event.key == pygame.K_DOWN or event.key == K_DOWN:
                     player.K_DOWN = True
                 if event.key == pygame.K_SPACE:
                     player.K_SPACE = True
             if event.type == pygame.KEYUP:
-                if event.key == pygame.K_LEFT:
+                if event.key == pygame.K_LEFT or event.key == K_LEFT:
                      player.K_LEFT = False
-                elif event.key == pygame.K_RIGHT:
+                elif event.key == pygame.K_RIGHT or event.key == K_RIGHT:
                      player.K_RIGHT = False
-                if event.key == pygame.K_UP:
+                if event.key == pygame.K_UP or event.key == K_UP:
                     player.K_UP = False
-                elif event.key == pygame.K_DOWN:
+                elif event.key == pygame.K_DOWN or event.key == K_DOWN:
                     player.K_DOWN = False
                 if event.key == pygame.K_SPACE:
                     player.K_SPACE = False
--- my_games/tilemap/player.py
+++ my_games/tilemap/player.py
@@ -1,14 +1,18 @@
 #AngelStreet @2021
 ####################################################
-import sys,os,pygame
+from utility import load_json, get_sprite, move_sprite
+import sys
+import os
+import pygame
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from utility import load_json,get_sprite,move_sprite
+
+
 class Player(pygame.sprite.Sprite):
-    def __init__(self,json,scale=1):
+    def __init__(self, json, scale=1):
         pygame.sprite.Sprite.__init__(self)
         self.json = json
         self.scale = scale
-        self.K_LEFT, self.K_RIGHT, self.K_DOWN, self.K_UP, self.K_SPACE = False, False, False,False, False
+        self.K_LEFT, self.K_RIGHT, self.K_DOWN, self.K_UP, self.K_SPACE = False, False, False, False, False
         self.current_frame = 0
         self.last_updated = 0
         self.velocity_x = 0
@@ -17,120 +21,141 @@ class Player(pygame.sprite.Sprite):
         self.init_player()
 
     def load_player_data_from_json(self):
-         data = load_json(self.json)
-         self.playersheet_name = data['player']['playersheet_name']
-         self.colorkey = data['player']['colorkey']
-         self.attributes = data['player']['attributes']
-         self.first_frame = data['player']['first_frame']
-         self.resolution = data['player']['resolution']
-         self.frames_data = data['player']['frames']
+        data = load_json(self.json)
+        self.playersheet_name = data['player']['playersheet_name']
+        self.colorkey = data['player']['colorkey']
+        self.attributes = data['player']['attributes']
+        self.first_frame = data['player']['first_frame']
+        self.resolution = data['player']['resolution']
+        self.frames_data = data['player']['frames']
 
-    def get_frame_sprite_data(self,frame) :
-        return frame['x'],frame['y'],frame['w'],frame['h'], frame['offsetx'],frame['offsety']
+    def get_frame_sprite_data(self, frame):
+        return frame['x'], frame['y'], frame['w'], frame['h'], frame['offsetx'], frame['offsety']
 
     def load_player_frames(self):
         flip_frames = []
-        for key,value in self.frames_data.items() :
-            flip_frame_name = key.replace('right','left')
+        for key, value in self.frames_data.items():
+            flip_frame_name = key.replace('right', 'left')
             flip_value = value.copy()
-            value.append('False') #identify frame to flip
+            value.append('False')  # identify frame to flip
             flip_value.append('True')
-            flip_frames.append((flip_frame_name,flip_value))
-        #Populate the dictionnary with flipped frames
-        for flip_frame_name,flip_value in flip_frames :
+            flip_frames.append((flip_frame_name, flip_value))
+        # Populate the dictionnary with flipped frames
+        for flip_frame_name, flip_value in flip_frames:
             self.frames_data[flip_frame_name] = flip_value
-        #Create spritesheets
+        # Create spritesheets
         self.playersheet_img = pygame.image.load(self.playersheet_name).convert_alpha()
-        self.playersheet_img_flip = pygame.transform.flip(self.playersheet_img.copy(),True, False)
-        #Create sprite for all aimation frames
-        for key, value in self.frames_data.items() :
+        self.playersheet_img_flip = pygame.transform.flip(self.playersheet_img.copy(), True, False)
+        # Create sprite for all aimation frames
+        for key, value in self.frames_data.items():
             midbottom = value[0]['midbottom']
-            flip  = value[-1]
-            for i, frame in enumerate(value) :
-                if i>0 and isinstance(frame,dict):
+            flip = value[-1]
+            for i, frame in enumerate(value):
+                if i > 0 and isinstance(frame, dict):
                     frame_name = frame['name']
-                    x,y,w,h,offsetx,offsety = self.get_frame_sprite_data(frame)
-                    if flip :
-                        sprite = get_sprite(self.playersheet_img_flip, x, y, w, h,self.colorkey)
-                    else :
-                        sprite = get_sprite(self.playersheet_img, x, y, w, h,self.colorkey)
+                    x, y, w, h, offsetx, offsety = self.get_frame_sprite_data(frame)
+                    if not flip:
+                        sprite = get_sprite(self.playersheet_img_flip, x, y, w, h, self.colorkey)
+                    else:
+                        sprite = get_sprite(self.playersheet_img, x, y, w, h, self.colorkey)
                     sprite = pygame.transform.scale(sprite, (w*self.scale, h*self.scale))
                     frame['sprite'] = sprite
 
-    def init_player(self) :
-       self.load_player_data_from_json()
-       self.image = pygame.Surface((self.resolution['max_w']*self.scale*2, self.resolution['max_h']*self.scale))
-       self.rect = self.image.get_rect()
-       self.image.set_colorkey(self.colorkey)
-       self.current_state = self.prev_state = self.first_frame
-       self.load_player_frames()
-
-
-    def reset_velocity(self) :
-        self.velocity_x,self.velocity_y = 0,0
-
-
-    def move(self,x,y):
-        move_sprite(self,x,y)
-
-    def move_player(self,) :
-        self.move(self.velocity_x,self.velocity_y + self.velocity_z)
-
-    def check_event(self) :
-        if self.K_SPACE:
-            self.reset_velocity()
-        else :
-            if self.K_LEFT:
-                self.velocity = -2
-            elif self.K_RIGHT:
-                self.velocity = 2
-            if self.K_UP:
-                self.velocity = -2
-            elif self.K_DOWN:
-                self.velocity = 2
-
     def set_state(self):
-        states = ['idle','walk','attack','jump','hurt']
-        for state in states :
-            if state in self.current_state :
+        states = ['idle', 'walk', 'attack', 'jump', 'hurt']
+        for state in states:
+            if state in self.current_state:
                 self.state = state
 
     def set_direction(self):
-      if 'right' in self.current_state :
-          self.direction = 'right'
-      else:  self.direction = 'left'
+        if 'right' in self.current_state:
+            self.direction_h = 'right'
+        else:
+            self.direction_h = 'left'
+        if 'up' in self.current_state:
+            self.direction_v = 'up'
+        else:
+            self.direction_v = 'down'
 
-    def get_frame_rate(self) :
-        frame_rate = self.frames_data[self.current_state][self.current_frame+1] #+1 to skip midbottm
+    def init_player(self):
+        self.load_player_data_from_json()
+        self.image = pygame.Surface(
+            (self.resolution['max_w']*self.scale*2, self.resolution['max_h']*self.scale))
+        self.rect = self.image.get_rect()
+        self.image.set_colorkey(self.colorkey)
+        self.current_state = self.prev_state = self.first_frame
+        self.load_player_frames()
+        self.set_state()
+        self.set_direction()
+
+    def reset_velocity(self):
+        self.velocity_x, self.velocity_y = 0, 0
+
+    def move(self, x, y):
+        move_sprite(self, x, y)
+
+    def move_player(self,):
+        self.move(self.velocity_x, self.velocity_y + self.velocity_z)
+
+    def check_event(self):
+        if self.K_SPACE:
+            self.reset_velocity()
+        else:
+            if self.K_LEFT:
+                self.velocity_x = -2
+                self.direction_h = 'left'
+            elif self.K_RIGHT:
+                self.velocity_x = 2
+                self.direction_h = 'right'
+            if self.K_UP:
+                self.velocity_y = -2
+                self.direction_v = 'up'
+            elif self.K_DOWN:
+                self.velocity_y = 2
+                self.direction_v = 'down'
+
+    def set_frame(self):
+        if self.velocity_x != 0:
+            self.current_state = "walk_%s_%s" % (self.direction_h,self.direction_v)
+        elif self.velocity_y != 0:
+            self.current_state = "walk_%s_%s" % (self.direction_h,self.direction_v)
+        elif self.state!='idle' and self.velocity_x==0 and self.velocity_x==0 and self.velocity_z == 0 :
+            self.current_state = "idle_%s_%s" % (self.direction_h,self.direction_v)
+
+    def get_frame_rate(self):
+        # +1 to skip midbottm
+        frame_rate = self.frames_data[self.current_state][self.current_frame+1]
         return frame_rate
 
-    def get_nb_frame(self) :
-        nb_frame = len(self.frames_data[self.current_state])-2 #less midbottom and flip
+    def get_nb_frame(self):
+        nb_frame = len(self.frames_data[self.current_state])-2  # less midbottom and flip
         return nb_frame
 
-    def get_current_sprite(self) :
-        sprite = self.frames_data[self.current_state][self.current_frame+1]['sprite'] #+1 to skip midbottom
-        return sprite
+    def display_current_sprite(self):
+        # print(self.frames_data[self.current_state][self.current_frame+1])
+        # +1 to skip midbottom
+        self.image = self.frames_data[self.current_state][self.current_frame+1]['sprite']
 
     def animate(self):
-        now = pygame.time.get_ticks()
         frame_rate = self.get_frame_rate()['frame_rate']
         nb_frames = self.get_nb_frame()
+        now = pygame.time.get_ticks()
         if now - self.last_updated > 100*frame_rate:
             self.last_updated = now
-            self.current_frame = (self.current_frame + 1) % len(nb_frames)
+            self.current_frame = (self.current_frame + 1) % nb_frames
 
-        if self.current_frame == 0 :
-            if 'attack'or 'jump'or 'hurt' in self.state :
+        if self.current_frame == 0:
+            if self.state in ('attack', 'jump', 'hurt'):
                 self.last_updated = now
                 self.current_state = self.prev_state
 
-        self.image =self.get_current_sprite()
+        self.display_current_sprite()
 
     def update(self):
         self.reset_velocity()
-        self.check_event() #check key press event
+        self.check_event()  # check key press event
         self.move_player()
-        self.set_state() #idle, move or attack
-        self.set_direction() #left or right
-        self.animate() #animate player by updating frame
+        self.set_frame()  # attack_right_down_1, hurt_right_up_1, etc..
+        self.set_state()  # idle, move or attack
+        self.set_direction()  # left or right
+        self.animate()  # animate player by updating frame
