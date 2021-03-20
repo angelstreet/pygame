16e94882f4e410b97ae0c7d6e221a4a655a4b659
joachim <joachim_djibril@hotmail.com>
2021-03-20 18:29:47 +0100

    optimizinf update player

--- my_games/tilemap/player.py
+++ my_games/tilemap/player.py
@@ -10,7 +10,7 @@ class Player(pygame.sprite.Sprite):
         self.json = json
         self.scale = scale
         self.K_LEFT, self.K_RIGHT, self.K_DOWN, self.K_UP, self.K_SPACE, self.K_RETURN = False, False, False, False, False, False
-        self.current_frame = 0
+        self.current_frame_id = 0
         self.last_updated = 0
         self.velocity = 2
         self.velocity_x = 0
@@ -19,6 +19,7 @@ class Player(pygame.sprite.Sprite):
         self.gravity = 1
         self.jump_force = -10
         self.z = 0
+        self.current_state = None
         self.init_player()
 
     def load_player_data_from_json(self):
@@ -36,7 +37,6 @@ class Player(pygame.sprite.Sprite):
     def load_player_frames(self):
         # Create spritesheets
         self.playersheet_img = pygame.image.load(self.playersheet_name).convert_alpha()
-        #self.playersheet_img_flip = pygame.transform.flip(self.playersheet_img.copy(), True, False)
         # Create sprite for all animation frames
         for key, value in self.frames_data.items():
             midbottom = value[0]['midbottom']
@@ -53,15 +53,43 @@ class Player(pygame.sprite.Sprite):
                     frame['sprite'] = sprite
                     frame['sprite_flip'] = sprite_flip
 
-    def set_current_state(self,state):
-        self.prev_state = self.current_state
-        self.current_state = state
+    def set_current_frame(self,frame):
+        #print(self.direction_h,self.current_frame,self.current_frame_id)
+        if not self.direction_h in self.current_frame:
+            self.current_frame_id=0
+        if self.prev_frame != self.current_frame:
+            self.prev_frame = self.current_frame
+            self.current_frame = frame
+            self.current_frame_id=0
 
-    def set_state(self):
-        states = ['idle', 'walk', 'attack', 'jump', 'hurt']
-        for state in states:
-            if state in self.current_state:
-                self.state = state
+
+    def set_direction(self):
+        if "down" in self.current_frame :
+            self.direction_v = "down"
+        else : self.direction_v = "up"
+        if "right" in self.current_frame :
+            self.direction_h = "right"
+        else  : self.direction_h = "left"
+
+    def set_frame(self):
+        print(self.current_state)
+        self.set_current_frame("%s_right_%s" % (self.current_state, self.direction_v))
+
+
+    def set_state(self,state=False):
+        if not self.current_state==state :
+            if state :
+                if self.current_state :
+                    self.prev_state = self.current_state
+                self.current_state = state
+            else :
+                states = ['idle', 'walk', 'attack', 'jump','fall', 'hurt']
+                for state in states:
+                    if state in self.current_frame:
+                        if self.current_state :
+                            self.prev_state = self.current_state
+                        self.current_state = state
+        self.set_frame()
 
     def init_player(self):
         self.load_player_data_from_json()
@@ -69,15 +97,12 @@ class Player(pygame.sprite.Sprite):
             (self.resolution['max_w']*self.scale*2, self.resolution['max_h']*self.scale))
         self.rect = self.image.get_rect()
         self.image.set_colorkey(self.colorkey)
-        self.current_state = self.prev_state = self.first_frame
-        self.load_player_frames()
+        self.prev_frame = None
+        self.current_frame = self.first_frame
+        self.set_direction()
         self.set_state()
-        if "down" in self.current_state :
-            self.direction_v = "down"
-        else : self.direction_v = "up"
-        if "right" in self.current_state :
-            self.direction_h = "right"
-        else  : self.direction_h = "left"
+        self.set_frame()
+        self.load_player_frames()
 
     def reset_velocity(self):
         self.velocity_x, self.velocity_y = 0, 0
@@ -87,16 +112,16 @@ class Player(pygame.sprite.Sprite):
 
     def move_player(self,):
         self.move(self.velocity_x, self.velocity_y + self.velocity_z)
-        if (self.state == 'jump' and self.velocity_z<0) :
+        if (self.current_state == 'jump' and self.velocity_z<0) :
             self.velocity_z = min(0,self.velocity_z+self.gravity)
             self.z+=self.velocity_z
-        elif(self.state == 'jump' and self.velocity_z==0) :
-            self.state="fall"
-        elif(self.state == 'fall') :
+        elif(self.current_state == 'jump' and self.velocity_z==0) :
+            self.set_state("fall")
+        elif(self.current_state == 'fall') :
             self.velocity_z = max(self.jump_force,self.velocity_z+self.gravity)
             self.z+=self.velocity_z
             if(self.velocity_z==-1*self.jump_force+self.gravity) :
-                self.state="idle"
+                self.set_state("idle")
                 self.velocity_z = 0
                 self.z=0
                 self.reset_velocity()
@@ -113,78 +138,73 @@ class Player(pygame.sprite.Sprite):
 
     def check_event(self):
         if self.K_RETURN:
-            self.reset_velocity()
-            self.state = 'attack'
-            self.attack()
+            if not self.current_state in ('attack'):
+                self.reset_velocity()
+                self.set_state('attack')
+                self.attack()
         elif self.K_SPACE:
-            if not self.state in ('jump','fall') :#need to be abuse for double jumps
-                self.state = 'jump'
+            if not self.current_state in ('jump','fall') :#need to be abuse for double jumps
+                self.set_state('jump')
                 self.jump()
-        elif not (self.state == 'attack' or self.state == 'jump' or self.state == 'hurt'):
+        elif not (self.current_state == 'attack' or self.current_state == 'jump' or self.current_state == 'fall' or self.current_state == 'hurt'):
             if self.K_LEFT:
                 self.velocity_x = -self.velocity
                 self.direction_h = 'left'
+                self.set_state('walk')
             elif self.K_RIGHT:
                 self.velocity_x = self.velocity
                 self.direction_h = 'right'
+                self.set_state('walk')
             if self.K_UP:
                 self.velocity_y = -self.velocity
                 self.direction_v = 'up'
+                self.set_state('walk')
             elif self.K_DOWN:
                 self.velocity_y = self.velocity
                 self.direction_v = 'down'
+                self.set_state('walk')
             if not (self.K_LEFT or self.K_RIGHT) :
                 self.velocity_x = 0
             if not (self.K_UP or self.K_DOWN) :
                 self.velocity_y = 0
-
-    def set_frame(self):
-        if self.state == 'attack' :
-            self.set_current_state("attack_right" % self.direction_v)
-        elif self.state == 'jump' :
-            self.set_current_state("jump_right_%s" % self.direction_v)
-        elif self.state == 'fall' :
-            self.set_current_state("fall_right_%s" % self.direction_v)
-        else:
-            if self.velocity_x != 0:
-                self.set_current_state("walk_right_%s" % self.direction_v)
-            elif self.velocity_y != 0:
-                self.set_current_state("walk_right_%s" % self.direction_v)
-            elif self.velocity_x==0 and self.velocity_x==0 and self.velocity_z == 0 :
-                self.set_current_state("idle_right_%s" % self.direction_v)
+            if not (self.K_LEFT or self.K_RIGHT or self.K_UP or self.K_DOWN) :
+                self.set_state('idle')
 
     def get_frame_rate(self):
         # +1 to skip midbottm
-        frame_rate = self.frames_data[self.current_state][self.current_frame+1]
+        frame_rate = self.frames_data[self.current_frame][self.current_frame_id+1]['frame_rate']
         return frame_rate
 
     def get_nb_frame(self):
-        nb_frame = len(self.frames_data[self.current_state])-2  # less midbottom and flip
-        return nb_frame
+        nb_frames = len(self.frames_data[self.current_frame])-1  # less midbottom and sprite
+        return nb_frames
 
     def display_current_sprite(self):
         if self.direction_h =='right' :
-            self.image = self.frames_data[self.current_state][self.current_frame+1]['sprite']
+            self.image = self.frames_data[self.current_frame][self.current_frame_id+1]['sprite']
         else:
-            self.image = self.frames_data[self.current_state][self.current_frame+1]['sprite_flip']
+            self.image = self.frames_data[self.current_frame][self.current_frame_id+1]['sprite_flip']
 
     def animate(self):
-        frame_rate = self.get_frame_rate()['frame_rate']
+        frame_rate = self.get_frame_rate()
         nb_frames = self.get_nb_frame()
         now = pygame.time.get_ticks()
         if now - self.last_updated > 100*frame_rate:
+            #if(self.current_state=='attack') :
+            #    print(self.current_frame_id)
             self.last_updated = now
-            self.current_frame = (self.current_frame + 1)
-            if self.current_frame == nb_frames :
-                self.current_frame = 0
-                if self.state in ('attack', 'hurt'):
+            self.current_frame_id += 1
+            if self.current_frame_id == nb_frames :
+                self.current_frame_id = 0
+                if self.current_state in ('attack', 'hurt'):
                     self.last_updated = now
-                    self.state = 'idle'
+                    self.set_state('idle')
+
         self.display_current_sprite()
 
     def update(self):
         self.check_event()  # check key press event
         self.move_player()
-        self.set_frame()  # attack_right_down_1, hurt_right_up_1, etc..
-        self.set_state()  # idle, move or attack
+        #self.set_frame()  # attack_right_down_1, hurt_right_up_1, etc..
+        #self.set_state()  # idle, move or attack
         self.animate()  # animate player by updating frame
